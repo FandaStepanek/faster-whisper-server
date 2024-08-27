31616fab60a846a57a4264ade1a07eb0c41e30fb
FandaStepanek <frantisek.stepanek@newtontech.cz>
2024-08-27 16:10:50 +0200

    Increase timetout for no data

--- faster_whisper_server/config.py
+++ faster_whisper_server/config.py
@@ -199,7 +199,7 @@ class Config(BaseSettings):
     """
     Maximum number of models that can be loaded at a time.
     """
-    max_no_data_seconds: float = 1.0
+    max_no_data_seconds: float = 10.0
     """
     Max duration to wait for the next audio chunk before transcription is finilized and connection is closed.
     """
